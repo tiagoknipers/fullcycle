b0f2aeaf9f206f751b4719dbdd0b5b3d5a0cf67a
Tiago Marcel Knipers <knipers@gmail.com>
2022-11-16 00:21:33 -0300

    desafio nginx-node

---
+++ docker/desafio-nginx-node/init-mysql.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS nodedb;
+
+USE nodedb;
+
+CREATE TABLE IF NOT EXISTS people (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ docker/desafio-nginx-node/node/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+/*
+let sql = "INSERT INTO people SET name = ?";
+connection.query(sql, [ random_name() ],  function(err, rows) {});
+connection.query(sql, [ random_name() ],  function(err, rows) {});
+connection.end()
+*/
+
+app.get('/', (req,res) => {
+    //res.send('<h1>Testando...</h1>')    
+    insertPeople(res);
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
+
+async function insertPeople(res) {    
+    var random_name = require('node-random-name');    
+    const connection = mysql.createConnection(config);
+    let sql = "INSERT INTO people SET name = ?";
+    connection.query(sql, [ random_name() ],  function(err, rows) {});
+    getPeople(res, connection);
+}
+  
+function getPeople(res, connection) {    
+    const sql = `SELECT id, name FROM people order by id`;  
+    
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        throw error
+      };
+      
+      let dados = '<p><ul>';
+      for(let people of results) {      
+        dados += `<li><b>${people.name}</b> (Id: ${people.id})</li>`;
+      }  
+      dados += '</ul></p>';
+      
+      res.send('<h1>FullCycle Rocks!</h1><p>Nomes cadastrados no banco de dados:</p>' + dados);    
+    });   
+
+    connection.end();
+}
